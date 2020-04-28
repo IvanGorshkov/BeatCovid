e66d0abc35b41076c716bb54e911cbe59e3e0916
Ilya <mozzzer@inbox.ru>
2020-04-28 17:24:06 +0300

    add classes for map

---
+++ cmake-build-debug/project/include/tinyxml/CMakeFiles/tinyxml.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ilya-kalinin/beatcovid/project/include/tinyxml/tinystr.cpp" "/home/ilya-kalinin/beatcovid/cmake-build-debug/project/include/tinyxml/CMakeFiles/tinyxml.dir/tinystr.cpp.o"
+  "/home/ilya-kalinin/beatcovid/project/include/tinyxml/tinyxml.cpp" "/home/ilya-kalinin/beatcovid/cmake-build-debug/project/include/tinyxml/CMakeFiles/tinyxml.dir/tinyxml.cpp.o"
+  "/home/ilya-kalinin/beatcovid/project/include/tinyxml/tinyxmlerror.cpp" "/home/ilya-kalinin/beatcovid/cmake-build-debug/project/include/tinyxml/CMakeFiles/tinyxml.dir/tinyxmlerror.cpp.o"
+  "/home/ilya-kalinin/beatcovid/project/include/tinyxml/tinyxmlparser.cpp" "/home/ilya-kalinin/beatcovid/cmake-build-debug/project/include/tinyxml/CMakeFiles/tinyxml.dir/tinyxmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "tinyxml_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../project/include"
+  "../project/include/tinyxml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cpp
@@ -0,0 +1,38 @@
+
+#include <SFML/Graphics.hpp>
+#include "Level_map.h"
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(480, 480), "SFML works!");
+    Texture t;
+    t.loadFromFile("ivan.png");
+    Sprite s;
+    s.setTexture(t);
+    s.setTextureRect(IntRect(0, 0, 64, 147));
+
+    Level lvl;
+    lvl.LoadFromFile("test_map.tmx");
+
+    Object pl = lvl.GetObject("player");
+    s.setPosition(pl.rect.left, (pl.rect.top - 147));
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color::White);
+        lvl.Draw(window);
+        window.draw(s);
+        window.display();
+    }
+
+    return 0;
+}
+
